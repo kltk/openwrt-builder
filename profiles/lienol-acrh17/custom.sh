d6c34129ece880cf49f374849c266771d819e962
z <haig8@msn.com>
2021-03-15 23:31:54 +0800

    add openclash

--- profiles/lienol-acrh17/custom.sh
+++ profiles/lienol-acrh17/custom.sh
@@ -3,3 +3,4 @@
 # clash_repo=https://github.com/frainzy1477/luci-app-clash
 # clash_repo=https://github.com/kltk/luci-app-clash
 clone https://github.com/kltk/luci-app-clash test package/luci-app-clash
+clone https://github.com/vernesong/OpenClash master package/luci-app-openclash
